25ce09ff71236521644506d3ece382ef15ff9d2e
AkshayaKrishna <akshayakrchalam@gmail.com>
2020-08-03 21:57:57 -0700

    First CPP Commit

---
+++ STEM-Summer-Workshop-C++/Basics/First.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "This is my first C++ program. I love coding!!" << endl;
+	return 0;
+}
---
+++ STEM-Summer-Workshop-C++/Basics/Hello World.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello to C++ Programming!!" << endl;
+	return 0;
+}
